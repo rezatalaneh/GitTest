3059ca41695923788a82c22ec96cfa2197a5d24b
Padashi Mobin <emobin.89@gmail.com>
2021-06-01 14:09:27 +0430

    love

--- app/src/main/java/ir/skygate/gittest/MainActivity.java
+++ app/src/main/java/ir/skygate/gittest/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
         String reza = "fershteh";
+        String love = "love";
     }
 
 }
